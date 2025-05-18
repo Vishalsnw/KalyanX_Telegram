7b45219e1220161b98f822379184e3de4c2899c1
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-05-18 13:25:54 +0530

    Update satta_prediction_advanced.py

--- satta_prediction_advanced.py
+++ satta_prediction_advanced.py
@@ -28,7 +28,7 @@ def send_telegram_message(message):
 
 # Data Load
 def load_data():
-    df = pd.read_csv("satta_data.csv")
+    df = pd.read_csv("enhanced_satta_data.csv")
     df.dropna(inplace=True)
     df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
     df = df.sort_values("Date")
