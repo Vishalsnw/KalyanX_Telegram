22f2b06bf0b2e272ce8c0ba38166c8c8cf5d8426
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-21 22:52:46 +0530

    Update satta_result_checker.py

--- satta_result_checker.py
+++ satta_result_checker.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 import os
 
 # --- Configuration ---
-TELEGRAM_BOT_TOKEN = "8050429062:AAGjX5t7poexZWjIEuMijQ1bVOJELqgdlmc"
+TELEGRAM_BOT_TOKEN = "8050429062:AAHfDWm42MvsfxVMOcOprH6vFxcisZJqOOg"  # ✅ Updated token
 CHAT_ID = "-1002573892631"
 
 CSV_FILE = "satta_data.csv"
